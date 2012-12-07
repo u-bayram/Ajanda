c26d5d30f220dc8b18502fe37582fabdbcba5374
Xenop <u.bayram54@gmail.com>
2012-12-08 01:06:22 +0200

    Yenilikler

--- src/agenda/model/User.java
+++ src/agenda/model/User.java
@@ -1,8 +1,32 @@
 package agenda.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "USER")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "USERID")
+	private Integer userId;
+	@Column(name = "USERNAME")
 	private String username;
+	@Column(name = "PASSWORD")
 	private String password;
+	@Column(name = "GENDER")
+	private Integer gender;
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
 
 	public String getUsername() {
 		return username;
@@ -19,4 +43,13 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
 }
--- src/agenda/swing/Deneme.java
+++ src/agenda/swing/Deneme.java
@@ -10,17 +10,24 @@ import agenda.service.UserService;
 
 public class Deneme {
 
-	/**
-	 * @param args
-	
+
 	private static final ApplicationContext ac = new ClassPathXmlApplicationContext(
 			"applicationContext.xml");
-
+	
 	public static void main(String[] args) {
 		System.out.println("Merhab");
 		UserService userService = (UserService) ac.getBean("userServiceImp");
 		List<User> users = userService.getSelectAll();
 		System.out.println(users.get(0).getUsername());
+		
+		User user = new User();
+		user.setUsername("Merve");
+		System.out.println(user.getUsername());
+		
+		
+		//User user1 = (User) ac.getBean("user");
+		//System.out.println(user1.getUsername());
+		
 	}
- */
+
 }
